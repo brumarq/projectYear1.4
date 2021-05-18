d3ab1de0e5c8ca9d1be77d7a80780335a722b208
AizazAhsan <aizazahsan23@gmail.com>
2021-05-18 19:19:12 +0200

    Empty base class, app.config file added (filled with db info), deleted 'class1.cs'

---
+++ ChapeauxDAL/Base.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace ChapeauxDAL
+{
+    public abstract class Base
+    {
+        public Base()
+        {
+            
+        }
+    }
+}
