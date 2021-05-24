aa1fa57447cdd0bf75a973f451ae636c3362c7a9
DennisDoksani <70758915+DennisDoksani@users.noreply.github.com>
2021-05-24 21:50:13 +0200

    Changed task form

--- ChapeauxUI/Program.cs
+++ ChapeauxUI/Program.cs
@@ -16,7 +16,7 @@ namespace ChapeauxUI
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new showdennis());
+            Application.Run(new KitchenDisplay());
         }
     }
 }
