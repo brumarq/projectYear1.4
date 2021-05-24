7ea71ea8195f53c1a7b18cc9d3218d786e974a75
REDitor <71357600+REDitor@users.noreply.github.com>
2021-05-24 14:11:47 +0200

    Created Application Structure (DRAFT)

---
+++ ChapeauxLogic/Order_Service.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauxLogic
+{
+    public class Order_Service
+    {
+    }
+}
---
+++ ChapeauxModel/Item.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChapeauxModel
+{
+    public class Item
+    {
+        public int ItemID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Category { get; set; }
+        public decimal VATRate { get; set; }
+        public bool menuType { get; set; }
+    }
+}
