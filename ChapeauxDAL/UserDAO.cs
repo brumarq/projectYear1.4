2192049ab2b06dfd18902d04670fa6516b0d5a57
AizazAhsan <aizazahsan23@gmail.com>
2021-05-25 12:36:11 +0200

    Creating branch before Pulling n Merging w/ Main

--- ChapeauxDAL/UserDAO.cs
+++ ChapeauxDAL/UserDAO.cs
@@ -1,10 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Data.SqlClient;
+using ChapeauxModel;
 
 namespace ChapeauxDAL
 {
     public class UserDAO : Base
     {
+        public List<User> Get_Users()
+        {
+            //read Users values from sql database
+            SqlCommand cmd = new SqlCommand
+                ("select userID, firstName, lastName, role, username, password from USERS");
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            List<User> users = new List<User>();
+
+            while (reader.Read())
+            {
+                User user = ReadUser(reader);
+                users.Add(user);
+            }
+            return users;
+        }
+
+        public User GetUserById(int userId)
+        {
+            conn.Open();
+            SqlCommand command = new SqlCommand
+                ("select userID, firstName, lastName, role, username, password from USERS where userId = @userID", conn);
+
+            command.Parameters.AddWithValue("@Id", userId);
+
+            SqlDataReader reader = command.ExecuteReader();
+
+            User user = null;
+            if (reader.Read())
+            {
+                user = ReadUser(reader);
+            }
+
+            reader.Close();
+            conn.Close();
+
+            return user;
+        }
+
+        private User ReadUser(SqlDataReader reader)
+        {   
+            User user = new User()
+            {   //retrieve data from all fields
+                UserID = (int)reader["userID"],
+                FirstName = reader["firstName"].ToString(),
+                LastName = reader["lastName"].ToString(),
+                Role = (Role)reader["role"],
+                LoginUsername = reader["username"].ToString(),
+                LoginPassword = reader["password"].ToString(),
+            };
+            return user;
+        }
     }
 }
--- ChapeauxLogic/User_Service.cs
+++ ChapeauxLogic/User_Service.cs
@@ -1,9 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using ChapeauxDAL;
+using ChapeauxModel;
+using System.Collections.Generic;
 
 namespace ChapeauxLogic
 {
     public class User_Service
     {
-        
+        private UserDAO userDAO = new UserDAO();
+
+        public List<User> GetUsers()
+        {
+            return userDAO.Get_Users();
+        }
+
+        public User GetUserById(int userId)
+        {
+            return userDAO.GetUserById(userId);
+        }
     }
 }
