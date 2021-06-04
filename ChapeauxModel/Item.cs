08ada33434f1a37c96cb448aec877a3c5b3db0a7
REDitor <71357600+REDitor@users.noreply.github.com>
2021-06-04 15:53:52 +0200

    double top decimal

--- ChapeauxModel/Item.cs
+++ ChapeauxModel/Item.cs
@@ -4,7 +4,7 @@
     {
         public int ItemID { get; set; }
         public string Name { get; set; }
-        public double Price { get; set; }
+        public decimal Price { get; set; }
         public int Stock { get; set; }
         public string Category { get; set; }
         public string Course { get; set; }
