0eeaf233cd0f58971a2250e0a95a1ccd1d10d48b
REDitor <71357600+REDitor@users.noreply.github.com>
2021-06-12 20:37:27 +0200

    added comments to getfood/getdrink

--- ChapeauxDAL/OrderItemDAL.cs
+++ ChapeauxDAL/OrderItemDAL.cs
@@ -32,7 +32,7 @@ namespace ChapeauxDAL
         #region Checkout
         public List<OrderItem> GetOrderFood(int orderID)
         {
-            string query = "SELECT ORDERITEMS.orderID, ORDERITEMS.[count], ORDERITEMS.itemID, ITEMS.[name], ITEMS.category, ITEMS.price, ITEMS.VATRate, ORDERITEMS.state " +
+            string query = "SELECT ORDERITEMS.orderID, ORDERITEMS.[count], ORDERITEMS.itemID, ITEMS.[name], ITEMS.category, ITEMS.price, ITEMS.VATRate, ORDERITEMS.state, ORDERITEMS.comments " +
                             "FROM ORDERITEMS " +
                             "INNER JOIN ORDERS ON ORDERS.orderID = ORDERITEMS.orderID " +
                             "INNER JOIN ITEMS ON ITEMS.itemID = ORDERITEMS.itemID " +
@@ -47,7 +47,7 @@ namespace ChapeauxDAL
 
         public List<OrderItem> GetOrderDrinks(int orderID)
         {
-            string query = "SELECT ORDERITEMS.orderID, ORDERITEMS.[count], ORDERITEMS.itemID, ITEMS.[name], ITEMS.category, ITEMS.price, ITEMS.VATRate, ORDERITEMS.state " +
+            string query = "SELECT ORDERITEMS.orderID, ORDERITEMS.[count], ORDERITEMS.itemID, ITEMS.[name], ITEMS.category, ITEMS.price, ITEMS.VATRate, ORDERITEMS.state, ORDERITEMS.comments " +
                             "FROM ORDERITEMS " +
                             "INNER JOIN ORDERS ON ORDERS.orderID = ORDERITEMS.orderID " +
                             "INNER JOIN ITEMS ON ITEMS.itemID = ORDERITEMS.itemID " +
@@ -75,6 +75,7 @@ namespace ChapeauxDAL
                      Price = (decimal)dr["price"],
                      VATRate = (decimal)dr["VATRate"],
                      State = (State)Enum.Parse(typeof(State), dr["state"].ToString()),
+                     Comment = (string)dr["comments"]
                 };
 
                 listOfItems.Add(orderItem);
